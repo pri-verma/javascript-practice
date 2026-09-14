2823944dd74e02c469a1b9da91bb21461bceb7f6
Priyanka Verma <noshimi14india@gmail.com>
2026-09-14 12:59:33 +0000

    Add JavaScript basics and type conversion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);  // while comparing null is treated as 0
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);      // undefined always gives false
+console.log(undefined == null);   // undefined gives true
+console.log(undefined > null); 
+console.log(undefined < null);
+
+
+// ===  use for Strict checking means datatype checking also while comparing
+
+console.log("2" === 2);
