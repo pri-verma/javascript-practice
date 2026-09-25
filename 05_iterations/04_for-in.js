15994bcabd086f7e2d92a0e864d9a0597275c0cf
Priyanka Verma <noshimi14india@gmail.com>
2026-09-25 14:58:05 +0000

    for in

--- 05_iterations/04_for-in.js
+++ 05_iterations/04_for-in.js
@@ -1,6 +1,35 @@
+// for in
+// for (const key in object) {
+//     if (!Object.hasOwn(object, key)) continue;
+    
+//     const element = object[key];
+    
+    
+// }
+
 const obj ={
     1 : "one",
     2 : "two",
     3 : "three",
     4 : "four"
+}
+
+for (const key in obj) {
+    console.log(key)        // return keys
+}
+
+for (const key in obj) {  
+    console.log(obj[key])   //return values
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+const arr = ["pri", "son", "jio"]
+
+for (const key in arr) {
+    console.log(key)     // return index key
+}
+
+for (const key in arr) {
+    console.log(arr[key])  // return element
 }
---
+++ 05_iterations/06_filter_map&reduce.js
@@ -0,0 +1,11 @@
+const coding = ["js", "py", "c++", "java", "rb"]
+
+const values = coding.forEach((item) => {
+    console.log(item);
+})
+console.log(values);    // return undefined
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const nums = [1,2,3,4,5,6,7,8,9,10]
+
