acea6a5384e857c201c6ac73c627f53b5ca89352
Priyanka Verma <noshimi14india@gmail.com>
2026-09-25 15:10:03 +0000

    filter

--- 05_iterations/06_filter_map&reduce.js
+++ 05_iterations/06_filter_map&reduce.js
@@ -2,6 +2,7 @@ const coding = ["js", "py", "c++", "java", "rb"]
 
 const values = coding.forEach((item) => {
     console.log(item);
+    return item;        // doesnot stores filtered values
 })
 console.log(values);    // return undefined
 
@@ -9,3 +10,5 @@ console.log(values);    // return undefined
 
 const nums = [1,2,3,4,5,6,7,8,9,10]
 
+const new_nums = nums.filter ( () => nums > 4)   //stores filtered values
+console.log(new_nums);
